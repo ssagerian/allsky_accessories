75c6f2323e825b4a18cf2d2fc35371c9cbcf55b3
steve sagerian <steve.sagerian@marmonfoodservice.com>
2023-12-27 12:55:53 -0600

    debugging si7020

--- si7020_I2C.py
+++ si7020_I2C.py
@@ -9,7 +9,8 @@ HUMIDITY_COMMAND = 0xF5
 
 class si7020_I2C:
     def __init__(self, address=0x40):
-        heater_map = { 0:"ON", 1:"OFF"}
+        heater_map = {0: "OFF", 1: "ON"}
+        vdd_map = {0: "OK", 1: "LOW!"}
         resolution_mapping = {
        	    (0, 0): "RH: 12 bit, Temp: 14 bit",
             (0, 1): "RH: 8 bit, Temp: 12 bit",
@@ -30,13 +31,13 @@ class si7020_I2C:
                 self.bus.write_byte_data(self.address, 0xE6, 0x04)  # heater on Temp Resolution == 12 bits, RH= 8 bits
                 time.sleep(1)
                 read_data = self.bus.read_byte_data(self.address, 0xE7)  # read register 1
-                heater_state =  heater_map.get(((read_data & 0x04)>>3),"?")
-                vdd_status = read_data & 0x40
+                heater_state = heater_map.get(((read_data & 0x04) >> 3), "?")
+                vdd_status = vdd_map.get(((read_data & 0x40) >> 5), "?")
                 rh_resolution = (read_data & 0x80) >> 6
                 temp_resolution = read_data & 0x01
                 resolution_description = resolution_mapping.get((rh_resolution, temp_resolution), "Unknown Resolution")
-                print( f"VDD status {vdd_status} si7020 heater status {heater_state} bit resolution {resolution_description}" )
-                syslog.syslog(syslog.LOG_INFO, f"VDD status {vdd_status} si7020 heater status {heater_state} bit resolution {resolution_description}")
+                print(f"Si7020: VDD  {vdd_status} heater {heater_state} bit resolution {resolution_description}")
+                syslog.syslog(syslog.LOG_INFO, f"Si7020:VDD {vdd_status} heater {heater_state} bit resolution {resolution_description}")
 
             except IOError as e:
                 print(f"1 Failed to initialize si7020 I2C device: {e}")
