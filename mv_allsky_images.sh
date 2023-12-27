1f8ada362e7f8f1b12a2dd74016c72c41dafa9f0
ssagerian <steven.sagerian@gmail.com>
2023-12-27 09:00:07 -0600

    improved script by checking for return results after scp

--- mv_allsky_images.sh
+++ mv_allsky_images.sh
@@ -10,11 +10,13 @@ destination_directory="pi@10.0.0.35:/mnt/homesky_ftp/images/"
 log_file="/var/log/allsky.log"
 
 # Find directories that are two days old or older
-find_result=$(find "$source_directory" -maxdepth 1 -type d -ctime +1)
+cd "$source_directory" 
+find_result=$(find . -maxdepth 1 -type d -ctime +1)
 
 # Check if any directories were found
 if [ -z "$find_result" ]; then
     echo "No directories found matching the criteria."
+    logger -p local0.info "No directories found matching transfer criteria: $dirpath"
     exit 0
 fi
 
@@ -25,9 +27,15 @@ while IFS= read -r dirpath; do
 
     # Use scp to transfer the contents of the directory to the remote server
     scp -r "$dirpath" "$destination_directory$dir_name"
+    
+    # Check the exit status of the scp command
+    if [ $? -ne 0 ]; then
+       logger -p local0.info "Transfer Failed $destination_directory$dir_name"
+       exit 1  # Exit with a non-zero status to indicate failure
+    fi
 
     # Optional: Remove the local directory after transfer
-    # rm -r "$dirpath"
+    rm -rf "$dirpath"
 
     # Log success
     logger -p local0.info "Transfer successful for directory: $dirpath"
